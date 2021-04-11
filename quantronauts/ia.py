5db40e1238750b8a66523f1713a11ec8e3c33f92
Mica <mika89700@gmail.com>
2021-04-11 15:35:32 +0200

    add ia grover

---
+++ quantronauts/ia.py
@@ -0,0 +1,74 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute
+
+
+def quantum_ia(sticks_root, backend_sim):
+    def diffuser(qc):
+        qc.h(qram_q)
+        qc.z(qram_q)
+        qc.cz(qram_q[0], qram_q[1])
+        qc.h(qram_q)
+
+    def qram(qc):
+        # 01
+        qc.x(qram_q[0])
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[0])
+        qc.x(qram_q[0])
+        # 10
+        qc.x(qram_q[1])
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[0])
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[1])
+        qc.x(qram_q[1])
+        # 11
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[0])
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[1])
+        qc.ccx(qram_q[0], qram_q[1], sticks_q[2])
+
+    qram_q = QuantumRegister(2, 'possibility')
+    sticks_q = QuantumRegister(4, 'sticks')
+    out_q = QuantumRegister(1, 'flag')
+    c = ClassicalRegister(2, "c")
+    qc = QuantumCircuit(qram_q, sticks_q, out_q, c)
+
+    qc.h(qram_q)
+    qc.x(out_q)
+    qc.h(out_q)
+    qc.barrier()
+
+    for i in range(1):
+        # Compute
+        # Init board
+        if sticks_root == 0:
+            qc.x(sticks_q[3])
+        for v in range(sticks_root):
+            qc.x(sticks_q[sticks_root - v])
+        # Qram
+        qram(qc)
+
+        # Flag
+        qc.mcx(sticks_q, out_q)
+
+        # Uncompute
+        # Qram
+        qram(qc)
+        # Init board
+        if sticks_root == 0:
+            qc.x(sticks_q[3])
+        for v in range(sticks_root):
+            qc.x(sticks_q[sticks_root - v])
+
+        # Apply generic diffuser
+        diffuser(qc)
+
+    qc.measure(qram_q, c)
+    # Interprete result
+    job = execute(qc, backend_sim, shots=512, memory=True)
+    result_job = job.result().get_counts()
+    result_memory = job.result().get_memory()
+    print("Memory ", result_memory)
+    if len(result_job) == 1:
+        to_return = int(result_memory[0], 2)
+        print("Return : ", to_return)
+    else:
+        to_return = 4
+
+    return to_return
--- quantronauts/nim_game.py
+++ quantronauts/nim_game.py
@@ -1,4 +1,5 @@
 import draw
+import ia
 from qiskit import Aer, QuantumCircuit, QuantumRegister, execute
 
 # Init Qasm simulator backend
@@ -11,15 +12,22 @@ player2 = [False, "Player2"]
 
 play = True
 
+computer = [False, False]
+nb_player = str(input("How many player ? 1/2 : "))
+if nb_player == '1':
+    computer[1] = True
+
+
 begin = str(input("Player1 start ? y/n : "))
 if begin == 'y':
     player1[0] = True
 else:
     player2[0] = True
+    computer[0] = True
 
 while play:
     # Generation of the first circuit with 11 qubits
-    stick = 11
+    stick = 4
     nb_qubit = stick
     drawing_add = ""
     draw.draw(stick, drawing_add)
@@ -33,32 +41,56 @@ while play:
             player_name = player1[1]
         else:
             player_name = player2[1]
-        
-        # Rule choice
-        print(player_name, "- You take : ")
-        nbstick = int(input())
-        for i in range(nbstick):
-            print(player_name, "- Which gate do you want to use on stick[", i+1, "] ?")
-            if stick-i == 1 and nb_qubit == 1:
-                gate = str(input("x : "))
-            elif stick-i == 1:
-                gate = str(input("x, cx : "))
-            elif stick-i == nb_qubit:
-                gate = str(input("h, x : "))
-            elif stick-i > 1:
-                gate = str(input("h, x, cx : "))
 
-            if gate == 'h':
-                qc_board.h(stick-(1+i))
-                drawing_add = "/ " + drawing_add
-            if gate == 'x':
-                qc_board.x(stick-(1+i))
+        # Player choice
+        if computer[1] is False or player1[0] is True:
+            print(player_name, "- You take : ")
+            nbstick = int(input())
+            for i in range(nbstick):
+                print(player_name, "- Which gate do you want to use on stick[", i + 1, "] ?")
+                if stick - i == 1 and nb_qubit == 1:
+                    gate = str(input("x : "))
+                elif stick - i == 1:
+                    gate = str(input("x, cx : "))
+                elif stick - i == nb_qubit:
+                    gate = str(input("h, x : "))
+                elif stick - i > 1:
+                    gate = str(input("h, x, cx : "))
+
+                if gate == 'h':
+                    qc_board.h(stick - (1 + i))
+                    drawing_add = "/ " + drawing_add
+                if gate == 'x':
+                    qc_board.x(stick - (1 + i))
+                    drawing_add = ""
+                if gate == 'cx':
+                    qc_board.cx(stick - i, stick - (1 + i))
+                    drawing_add = "¬ " + drawing_add
+
+        # Computer choice
+        if computer[0] is True and computer[1] is True:
+            nbstick = 0
+            if stick == 1:
+                qc_board.x(stick - 1)
                 drawing_add = ""
-            if gate == 'cx':
-                qc_board.cx(stick-i, stick-(1+i))
-                drawing_add = "¬ " + drawing_add
+                nbstick = 1
+            else:
+                result_analyse = ia.quantum_ia(stick % 4, backend_sim)
+                if result_analyse == 4:
+                    for i in range(3):
+                        qc_board.h(stick - (1 + i))
+                        qc_board.cx(stick - i, stick - (1 + i))
+                        qc_board.x(stick - (1 + i))
+                        drawing_add = ""
+                        nbstick = 3
+                else:
+                    for i in range(result_analyse):
+                        qc_board.x(stick - (1 + i))
+                        print("Coucou i:",i)
+                    nbstick = result_analyse
+
         stick -= nbstick
-        
+
         # Check if board is clean
         if stick < 1:
             # Run circuit
@@ -67,31 +99,33 @@ while play:
             job = execute(qc_board, backend_sim, shots=1, memory=True)
             result_memory = job.result().get_memory()
             result = result_memory[0]
-            print("Result : ", result)
             for i in range(len(result)):
-                print("[", i, "]:", result[i])
                 if result[i] == '0':
                     stick += 1
             print("stick left : ", stick)
-            
+
             # Check circuit result
             if stick < 1:
                 if player1[0]:
                     print("\n\n##################\n  Player 2 win !\n##################")
                 if player2[0]:
                     print("\n\n##################\n  Player 1 win !\n##################")
+                if computer[0] is False and computer[1] is True:
+                    print("\n\n##################\n  Machine win !\n##################")
             else:
                 # Generation of new circuit with qubits left
                 nb_qubit = stick
                 drawing_add = ""
                 stick_qubit = QuantumRegister(nb_qubit, 'stick')
                 qc_board = QuantumCircuit(stick_qubit)
-        
+
         # Inverse turn
         if stick > 0:
             draw.draw(stick, drawing_add)
             player2[0] = not player2[0]
             player1[0] = not player1[0]
+            if computer[1]:
+                computer[0] = not computer[0]
 
     # New game ?
     again = str(input("Play again ? (y/n) --> "))
