660b848231fb40c8004d8e2b7023559c512075ae
Michaël <michael.rollin@protonmail.com>
2021-06-24 18:35:07 +0200

    Update nim_game.py

--- quantronauts/nim_game.py
+++ quantronauts/nim_game.py
@@ -10,12 +10,14 @@ backend_sim = qasm
 player1 = [False, "Player1"]
 player2 = [False, "Player2"]
 
+print("""
 ###################################################
 # Welcome in the Quantum Nim Game - CLI
 # Will you succeed to win against the Quantum AI ?
 #
 # Good Luck !!!
 ###################################################
+""")
 
 play = True
 
